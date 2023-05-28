54d4d5c3a13a03fdc95f03582d4fac510ff751bb
up1 <somkiat.p@gmail.com>
2023-05-28 16:57:34 +0700

    Change column names

--- catalog/catalog/Product.cs
+++ catalog/catalog/Product.cs
@@ -8,8 +8,8 @@ namespace catalog
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public int price { get; set; }
-        public int stock { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
     }
 }
 
--- database/setup.sql
+++ database/setup.sql
@@ -4,8 +4,8 @@ CREATE TABLE Products (
     ID int, 
     Name nvarchar(max),
     Description nvarchar(max),
-    price int,
-    stock int);
+    Price int,
+    Stock int);
 GO
 INSERT INTO PRODUCTS VALUES(1, "Name 01", "Desc 01", 0,0);
 GO
