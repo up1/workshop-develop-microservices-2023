d0f1e392275c815ff17c0afc3f6883dd82797c00
up1 <somkiat.p@gmail.com>
2023-05-28 17:06:30 +0700

    Change table name

--- database/setup.sql
+++ database/setup.sql
@@ -1,13 +1,13 @@
 USE catalog-api;
 GO
-CREATE TABLE Products (
+CREATE TABLE Product (
     ID int, 
     Name nvarchar(max),
     Description nvarchar(max),
     Price int,
     Stock int);
 GO
-INSERT INTO PRODUCTS VALUES(1, "Name 01", "Desc 01", 0,0);
+INSERT INTO Product VALUES(1, "Name 01", "Desc 01", 0,0);
 GO
-INSERT INTO PRODUCTS VALUES(2, "Name 02", "Desc 02", 0,0);
+INSERT INTO Product VALUES(2, "Name 02", "Desc 02", 0,0);
 GO
