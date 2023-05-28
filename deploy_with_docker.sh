2857954ab6f371115365db09e3d90a5fb81ab5cd
up1 <somkiat.p@gmail.com>
2023-05-28 16:52:57 +0700

    Change database name

--- deploy_with_docker.sh
+++ deploy_with_docker.sh
@@ -23,9 +23,9 @@ done
 echo "Intial database for testing ..."
 DB_USER=SA
 DB_PASSWORD=zitgmLwmp1@q
-DB_NAME=catalog-api
+DB_NAME=catalog_api
 docker container exec database /opt/mssql-tools/bin/sqlcmd -U "$DB_USER" -P "$DB_PASSWORD" -Q "CREATE DATABASE $DB_NAME;"
-docker container exec database /opt/mssql-tools/bin/sqlcmd -U "$DB_USER" -P "$DB_PASSWORD" -d master -i /app/setup.sql;"
+docker container exec database /opt/mssql-tools/bin/sqlcmd -U "$DB_USER" -P "$DB_PASSWORD" -d master -i /app/setup.sql
 
 echo "Start stock service"
 docker compose -f $FILE up -d stock
