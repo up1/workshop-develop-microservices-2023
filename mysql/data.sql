c3512dc9433f6fbde4420b3f3da5227d77675338
up1 <somkiat.p@gmail.com>
2023-05-29 21:10:01 +0700

    Add mysql database

---
+++ mysql/data.sql
@@ -0,0 +1,4 @@
+insert into product_price values (1, 100);
+insert into product_price values (2, 200);
+insert into product_stock values (1, 11);
+insert into product_stock values (2, 12);
---
+++ mysql/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE product_price (
+  product_id INT(11),
+  price INT(11)
+);
+
+CREATE TABLE product_stock (
+  product_id INT(11),
+  stock INT(11)
+);
