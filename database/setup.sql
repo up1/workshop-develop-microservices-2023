40b6ff24752ee3c820757e7b5967d823e934ad9a
up1 <somkiat.p@gmail.com>
2023-05-29 22:19:15 +0700

    Add data

--- database/setup.sql
+++ database/setup.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE catalog_api;
-GO
+-- CREATE DATABASE catalog_api;
+-- GO
 USE catalog_api;
 GO
-CREATE TABLE Product (
-    ID int, 
-    Name nvarchar(max),
-    Description nvarchar(max),
-    Price int,
-    Stock int);
-GO
+-- CREATE TABLE Product (
+--     ID int, 
+--     Name nvarchar(max),
+--     Description nvarchar(max),
+--     Price int,
+--     Stock int);
+-- GO
 INSERT INTO Product VALUES(1, "Name 01", "Desc 01", 0,0);
 INSERT INTO Product VALUES(2, "Name 02", "Desc 02", 0,0);
 GO
